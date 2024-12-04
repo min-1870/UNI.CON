e0b38545252bad445bd85b53ef2dd35c27be97fa
Minseok Kim <200134kms@gmail.com>
2024-12-05 01:11:19 +1100

    cors setting for the server

--- backend/src/src/settings.py
+++ backend/src/src/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Frontend URL
 ]
 
 ROOT_URLCONF = 'src.urls'
