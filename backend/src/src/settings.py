1b5fd3ced4c20cef85cedccc6eff884e12fe60c4
EC2 Default User <ec2-user@ip-172-31-9-234.ap-southeast-2.compute.internal>
2024-12-29 16:30:07 +0000

    fix setting in django

--- backend/src/src/settings.py
+++ backend/src/src/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gg+6bb-1cnj$m!pr)!_bit=+7d$p1zqa2(6*84&m-e+%^*r+^y'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = ENV_DEBUG
