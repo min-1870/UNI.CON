7f549a950372a4c3cf9b3c8e2e3d3575e686eca5
Minseok Kim <200134kms@gmail.com>
2025-01-18 00:31:37 +1100

    modify debug domain for frontend

--- frontend/src/constants.jsx
+++ frontend/src/constants.jsx
@@ -1,1 +1,8 @@
-export const API_URL = "https://unicon.min1870.com/api";
+const DEBUG = false
+
+let domain = "https://unicon.min1870.com/api"
+if (DEBUG) {
+    domain = "http://localhost:8000/api";
+}
+
+export const API_URL = domain;
