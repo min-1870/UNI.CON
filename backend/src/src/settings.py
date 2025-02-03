08671771b9207e66e22ce1a000cb1d5dd53b3211
Minseok Kim <200134kms@gmail.com>
2025-02-03 12:00:39 +1100

    hotfix

--- backend/src/src/settings.py
+++ backend/src/src/settings.py
@@ -53,7 +53,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
