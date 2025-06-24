2a6968f891ba4781b3912d44736a68acfc272ed3
Minseok Kim <200134kms@gmail.com>
2025-06-24 15:19:07 +1000

    fix view error in the api

--- backend/src/community/utils/article_helpers.py
+++ backend/src/community/utils/article_helpers.py
@@ -8,7 +8,7 @@ from community.constants import (
     PAGINATOR_SIZE,
     CACHE_TIMEOUT,
 )
-from community.models import Article, ArticleUser, ArticleTag, ArticleLike, ArticleSave, Tag
+from community.models import Article, ArticleUser, ArticleTag, ArticleLike, ArticleSave, Tag, ArticleView
 from community.utils.database_utils import get_faiss_index, search_similar_embeddings
 from .response_serializers import ArticleResponseSerializer
 from django.contrib.postgres.aggregates import ArrayAgg
@@ -161,7 +161,7 @@ def get_paginated_articles(request, queryset, sort_by, cache_key, embedding_vect
     cache_key = ARTICLE_USER_VIEWED_UNSORTED_IDS_CACHE_KEY(user_instance.id)
     user_viewed_articles = cache.get(cache_key, None)
     if user_viewed_articles is None:
-        user_viewed_articles = ArticleLike.objects.filter(user=user_instance).values_list(
+        user_viewed_articles = ArticleView.objects.filter(user=user_instance).values_list(
             "article", flat=True
         )
         user_viewed_articles = {pk: True for pk in user_viewed_articles}
@@ -245,6 +245,9 @@ def get_serialized_article(request, article_instance):
     
     cache_key = ARTICLE_USER_SAVED_UNSORTED_IDS_CACHE_KEY(user_instance.id)
     user_saved_articles = cache.get(cache_key, None)
+    
+    cache_key = ARTICLE_USER_VIEWED_UNSORTED_IDS_CACHE_KEY(user_instance.id)
+    user_viewed_articles = cache.get(cache_key, None)
 
     # If the cache miss fetch them
     if user_liked_articles is None:
@@ -266,6 +269,16 @@ def get_serialized_article(request, article_instance):
 
     save_status = user_saved_articles.get(article_instance.id, False)
     serialized_annotated_article["save_status"] = save_status
+
+    if user_viewed_articles is None:
+        user_viewed_articles = ArticleView.objects.filter(user=user_instance).values_list(
+            "article", flat=True
+        )
+        user_viewed_articles = {pk: True for pk in user_viewed_articles}
+        cache.set(cache_key, user_viewed_articles, CACHE_TIMEOUT)
+
+    view_status = user_viewed_articles.get(article_instance.id, False)
+    serialized_annotated_article["view_status"] = view_status
 
     return serialized_annotated_article
 
