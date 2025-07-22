c3ccd3cfa2510a55bbc9238183c0312292b9615c
Minseok Kim <200134kms@gmail.com>
2025-07-23 01:35:08 +1000

    hotfix

--- backend/src/src/settings.py
+++ backend/src/src/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG").lower() == "true"
 DEMO = config("DEMO").lower() == "true" 
 # HTTPS Settings
-if DEBUG and not DEMO:
+if DEBUG :
     ALLOWED_HOSTS = []
     CORS_ALLOW_ALL_ORIGINS = True
 else:
