3d2748732ca37d896db22cb60a46070e3ad51b43
Minseok Kim <200134kms@gmail.com>
2025-07-24 04:28:32 +1000

    fix docker files

--- backend/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-cat > entrypoint.sh <<'EOF'
-#!/bin/sh
-# apply migrations
-python src/manage.py migrate --noinput
-
-# then hand off to whatever CMD was given
-exec "$@"
-EOF
--- backend/src/settings.py
+++ backend/src/settings.py
@@ -232,3 +232,28 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.User'
+
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        # Django request logger
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+        # Your app code
+        "": {
+            "handlers": ["console"],
+            "level": "INFO",  # or DEBUG
+        },
+    },
+}
