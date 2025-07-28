586ae535725aac23c29dd1cfe7ca4d498b859c74
Minseok Kim <200134kms@gmail.com>
2025-07-29 01:00:23 +1000

    fix docker-compose file for debug mode

--- backend/community/utils/__init__.py
+++ backend/community/utils/__init__.py
@@ -31,7 +31,6 @@ from .database_utils import (
     get_faiss_index,
     update_sorted_ids_cache,
     update_unsorted_ids_cache,
-    DummyRedis
 )
 
 from .response_serializers import ArticleResponseSerializer, CommentResponseSerializer
